8ef0b47a550ac721c6323f87b15549cea7b7d9c2
lamast <stoeva.lana@gmail.com>
2022-12-11 14:52:55 +0200

    feat: added invoices repository

---
+++ src/main/java/com/mycompany/swingapp/data/InvoicesRepository.java
@@ -0,0 +1,37 @@
+package com.mycompany.swingapp.data;
+
+import com.mycompany.swingapp.data.Connect;
+import java.util.ArrayList;
+import com.mycompany.swingapp.models.InvoicesEntity;
+
+/**
+ *
+ * @author Ruzolda
+ */
+public class InvoicesRepository {
+
+    private static final String INVOICES_TABLE = "Invoices";
+    private static final String ID_COLUMN = "id_invoice";
+    private static final String ID_EMP_COLUMN = "id_employees";
+    private static final String ID_SERVICE_COLUMN = "id_service";
+    private static final String STATUS_COLUMN = "statys";
+
+    
+    private final Connect query;
+
+    public InvoicesRepository(Connect query) {
+        this.query = query;
+    }
+
+    public ArrayList<InvoicesEntity> getAllInvoices() {
+        ArrayList result = query.select(new String[]{ID_COLUMN, ID_EMP_COLUMN, ID_SERVICE_COLUMN, STATUS_COLUMN}, INVOICES_TABLE);
+        ArrayList<InvoicesEntity> invoices = new ArrayList<InvoicesEntity>();
+
+        for (int i = 0; i < result.size(); i++) {
+            String[] row = result.get(i).toString().split("---");
+            invoices.add(new InvoicesEntity(row[0], row[1], row[2], row[3]));
+        }
+
+        return invoices;
+    }
+}
---
+++ src/main/java/com/mycompany/swingapp/models/InvoicesEntity.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.swingapp.models;
+
+/**
+ *
+ * @author Ruzolda
+ */
+public class InvoicesEntity {
+    private String invoiceId;
+    private String employeeId;
+    private String serviceId;
+    private String status;
+
+    public InvoicesEntity(String invoiceId, String employeeId, String serviceId, String status) {
+        this.invoiceId = invoiceId;
+        this.employeeId = employeeId;
+        this.serviceId = serviceId;
+        this.status = status;
+    }
+
+    
+    public String getInvoiceId() {
+        return invoiceId;
+    }
+
+    public void setInvoiceId(String invoiceId) {
+        this.invoiceId = invoiceId;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getServiceId() {
+        return serviceId;
+    }
+
+    public void setServiceId(String serviceId) {
+        this.serviceId = serviceId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+    
+}
